eee38099468bd34d673e84c65ddbf438c37a3be5
riyasunil <riaelisabeths@gmail.com>
2024-12-12 15:26:52 +0530

    finally overrid global css

--- src/components/BlogDetail.vue
+++ src/components/BlogDetail.vue
@@ -1,35 +1,44 @@
 <template>
   <div class="flex flex-col gap-6">
-    <!-- Blog Title -->
     <h1 class="text-[#FF9EB5] text-5xl font-bold">{{ blog.title }}</h1>
-
-    <!-- Dynamic HTML Content -->
-    <MyDynamicComponent tag="div" :html="blog.content" class="content" />
-
-    <div></div>
+    <div class="content" v-html="blog.content"></div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-import { useRoute } from 'vue-router';
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import { useRoute } from 'vue-router'
 import DOMPurify from 'dompurify';
-import MyDynamicComponent from './MyDynamicComponent.vue';
 
-const route = useRoute();
-const blog = ref({});
+
+const route = useRoute()
+const blog = ref({})
 
 const fetchBlog = async () => {
   const response = await axios.get(`${import.meta.env.VITE_BASE_URL}api/blogs/${route.params.id}/`);
-  
-  // Sanitize content to prevent XSS attacks
   const sanitizedContent = DOMPurify.sanitize(response.data.content);
-  
-  // Update the blog object with sanitized content
   blog.value = { ...response.data, content: sanitizedContent };
-};
+  // blog.value = response.data
+}
 
-onMounted(fetchBlog);
+onMounted(fetchBlog)
 </script>
 
+<style>
+.content h1 {
+  font-size: 32px;
+}
+.content h2 {
+  font-size: 24px;
+}
+.content h3 {
+  font-size: 18px;
+}
+.content h4 {
+  font-size: 16px;
+}
+.content p {
+  font-size: 14px;
+}
+</style>
